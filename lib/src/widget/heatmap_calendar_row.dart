3003807e719c31ec0e44511f821420d5ad57b5ec
dorvio <72299085+dorvio@users.noreply.github.com>
2023-12-08 21:18:00 +0100

    Update heatmap_calendar_row.dart

--- lib/src/widget/heatmap_calendar_row.dart
+++ lib/src/widget/heatmap_calendar_row.dart
@@ -98,7 +98,7 @@ class HeatMapCalendarRow extends StatelessWidget {
             date: DateTime(
                 startDate.year,
                 startDate.month,
-                startDate.day - startDate.weekday % 7 + i),
+                startDate.day - startDate.weekday % 7 + i - 1),
             backgroundColor: defaultColor,
             size: size,
             fontSize: fontSize,
