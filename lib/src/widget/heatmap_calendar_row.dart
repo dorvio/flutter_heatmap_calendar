45f1e82e43d2907c019e894217102731886a5733
dorvio <72299085+dorvio@users.noreply.github.com>
2023-12-08 21:53:44 +0100

    Update heatmap_calendar_row.dart

--- lib/src/widget/heatmap_calendar_row.dart
+++ lib/src/widget/heatmap_calendar_row.dart
@@ -88,7 +88,7 @@ class HeatMapCalendarRow extends StatelessWidget {
             i < (startDate.weekday % 7) - 1) ||
         (endDate == DateUtil.endDayOfMonth(endDate) &&
             endDate.day - startDate.day != 7 &&
-            i > (startDate.weekday % 7))
+            i <= (endDate.weekday % 7)))
         ? Container(
             width: size ?? 42,
             height: size ?? 42,
@@ -98,7 +98,7 @@ class HeatMapCalendarRow extends StatelessWidget {
             date: DateTime(
                 startDate.year,
                 startDate.month,
-                startDate.day - startDate.weekday % 7 + i + 1 + (i >= startDate.weekday % 7)),
+                startDate.day - startDate.weekday % 7 + i + 1),
             backgroundColor: defaultColor,
             size: size,
             fontSize: fontSize,
@@ -109,7 +109,7 @@ class HeatMapCalendarRow extends StatelessWidget {
             selectedColor: datasets?.keys.contains(DateTime(
                     startDate.year,
                     startDate.month,
-                    startDate.day - startDate.weekday % 7 + i)) ??
+                    startDate.day - startDate.weekday % 7 + i + 1)) ??
                 false
                 ? colorMode == ColorMode.opacity
                     ? colorsets?.values.first.withOpacity((datasets?[
